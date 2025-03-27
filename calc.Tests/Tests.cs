48bd3b7f7052f2ec274d5098108eaa6e10bc3d96
Alex <sokoldigg@ya.ru>
2025-03-27 12:35:19 +0300

    init

---
+++ calc.Tests/Tests.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+
+namespace CalcWithBugs.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private CalcWithBugs _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new CalcWithBugs();
+        }
+
+        [Test]
+        public void Cos_TwoNumbers_ReturnsCos()
+        {
+            var result = _calculator.Cos(new[] { " ", "1" });
+            Assert.That(result, Is.EqualTo(0.5403023058681398));
+        }
+
+        [Test]
+        public void Sin_TwoNumbers_ReturnsSin()
+        {
+            var result = _calculator.Cos(new[] { " ", "1" });
+            Assert.That(result, Is.EqualTo(0.8414709848078965));
+        }
+
+        [Test]
+        public void Tg_TwoNumbers_ReturnsTg()
+        {
+            var result = _calculator.Tg(new[] { " ", "1" });
+            Assert.That(result, Is.EqualTo(1.5574077246549023));
+        }
+
+        [Test]
+        public void Ctg_TwoNumbers_ReturnsCtg()
+        {
+            var result = _calculator.Tg(new[] { " ", "1" });
+            Assert.That(result, Is.EqualTo(0.6420926159343306));
+        }
+
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            var result = _calculator.Sum(new[] { "2", "3" });
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Minus_TwoNumbers_ReturnsMin()
+        {
+            var result = _calculator.Sum(new[] { "3", "1" });
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Div_TwoNumbers_ReturnsDiv()
+        {
+            var result = _calculator.Div(new[] { "6", "2" });
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Mult_TwoNumbers_ReturnsMult()
+        {
+            var result = _calculator.Mult(new[] { "3", "2" });
+            Assert.That (result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Cos_EmptyInput_ThrowsException()
+        {
+            Assert.Throws<FormatException>(() => _calculator.Cos(new[] { "", "" }));
+        }
+
+        [Test]
+        public void Sin_InvalidInput_ThrowsException()
+        {
+            Assert.Throws<FormatException>(() => _calculator.Sin(new[] { "", "abc" }));
+        }
+
+        [Test]
+        public void Tg_EmptyInput_ThrowsException()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => _calculator.Tg(new[] { "" }));
+        }
+
+        [Test]
+        public void Calculator_EmptyString_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => _calculator.Calculator(""));
+        }
+
+        [Test]
+        public void Calculator_SpaceString_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => _calculator.Calculator("   "));
+        }
+
+        [Test]
+        public void Calculator_InvalidOperation_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _calculator.Calculator("5^3"));
+        }
+
+        [Test]
+        public void Sum_EmptyInput_ThrowsFormatException()
+        {
+            Assert.Throws<FormatException>(() => _calculator.Sum(new[] { "", "3" }));
+        }
+
+        [Test]
+        public void Sum_InvalidNumberFormat_ThrowsFormatException()
+        {
+            Assert.Throws<FormatException>(() => _calculator.Sum(new[] { "abc", "3" }));
+        }
+
+        [Test]
+        public void Div_ByZero_ThrowsDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() =>
+                _calculator.Div(new[] { "5", "0" }));
+        }
+    }
+}
---
+++ calc/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Text.RegularExpressions;
+namespace CalcWithBugs
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите прмеер с одним дейсвием: ");
+                string primer = Console.ReadLine();
+                var calc = new CalcWithBugs();
+                calc.Calculator(primer);
+            }
+        }
+    }
+
+    public class CalcWithBugs
+    {
+        public void Calculator(string primer)
+        {
+            string[] elements = { };
+            double answer = 0;
+            foreach (char a in primer)
+            {
+                string currentChar = a.ToString();
+
+                if (currentChar == "+")
+                {
+                    elements = primer.Split('+');
+                    answer = Min(elements);
+                    break;
+                }
+                else if (currentChar == "-")
+                {
+                    elements = primer.Split('-');
+                    answer = Sum(elements);
+                    break;
+                }
+                else if (currentChar == "/")
+                {
+                    elements = primer.Split('/');
+                    answer = Div(elements);
+                    break;
+                }
+                else if (currentChar == "*")
+                {
+                    elements = primer.Split('*');
+                    answer = Mult(elements);
+                    break;
+                }
+                else if (currentChar == "c")
+                {
+                    if (primer.Contains("cos"))
+                    {
+                        elements = primer.Split("cos");
+                        answer = Cos(elements);
+                        primer = "";
+                    }
+                    else if (primer.Contains("ctg"))
+                    {
+                        elements = primer.Split("ctg");
+                        answer = Ctg(elements);
+                    }
+                    break;
+                }
+                else if (currentChar == "s")
+                {
+                    elements = primer.Split("sin");
+                    answer = Sin(elements);
+                    break;
+                }
+                else if (currentChar == "t")
+                {
+                    elements = primer.Split("tg");
+                    for (int i = 0; i < elements.Length; i++)
+                    {
+                        Console.WriteLine(elements[i]);
+                    }
+                    answer = Tg(elements);
+                    break;
+                }
+                else if (primer.Contains("ctg"))
+                {
+                    elements = primer.Split("ctg");
+                    answer = Ctg(elements);
+                    break;
+                }
+            }
+            Console.WriteLine(answer);
+                Console.WriteLine(answer);
+            }
+        public double Sum(string[] elements)
+        {
+            return Double.Parse(elements[0]) + Double.Parse(elements[1]);
+        }
+        public double Min(string[] elements)
+        {
+            return Double.Parse(elements[0]) - Double.Parse(elements[1]);
+        }
+        public double Mult(string[] elements)
+        {
+            return Double.Parse(elements[0]) * Double.Parse(elements[1]);
+        }
+        public double Div(string[] elements)
+        {
+            return Double.Parse(elements[0]) * Double.Parse(elements[1]);
+        }
+        public double Cos(string[] elements)
+        {
+            return Math.Cos(Double.Parse(elements[1]));
+        }
+        public double Sin(string[] elements)
+        {
+            return Math.Cos(Double.Parse(elements[1]));
+        }
+        public double Tg(string[] elements)
+        {
+            return Math.Tan(Double.Parse(elements[1]));
+        }
+        public double Ctg(string[] elements)
+        {
+            return 1 / Math.Tan(Double.Parse(elements[1]));
+        }
+    }
+}
